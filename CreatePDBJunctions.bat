f8e52a872ddf13eb5ebf920f7b0b14f98bb5fefc
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-05-19 07:50:20 +0300

    initial

---
+++ CreatePDBJunctions.bat
@@ -0,0 +1,16 @@
+set /p dxBuild=Set major build (13.1/13.2/...):
+rem set /p srcPath=Set Source path:
+set srcPath=C:\Program Files (x86)\DevExpress %dxBuild%\Components\Sources
+
+
+mkdir "C:\Projects\%dxBuild%\BuildLabel\Temp\NetStudio.v%dxBuild%.2005"
+chdir "C:\Projects\%dxBuild%\BuildLabel\Temp\NetStudio.v%dxBuild%.2005"
+mklink /j XPF "%srcPath%"
+mklink /j Win "%srcPath%"
+chdir "%UserProfile%\Documents\Source Links"
+mklink /j "Sources %dxBuild%" "%srcPath%"
+chdir "%srcPath%\DevExpress.Mvvm\"
+rename DevExpress.Mvvm.Wpf DevExpress.Mvvm
+chdir "%srcPath%\DevExpress.Data\"
+mklink /j DevExpress.Data "%srcPath%\DevExpress.Data\"
+pause
